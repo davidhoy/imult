b1cf6f7a0205f1a845973761dd6d83f0d7e0f13d
David Hoy <david@thehoys.com>
2019-09-18 17:04:13 -0400

    Initial Commit

---
+++ imult/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  imult
+//
+//  Created by David Hoy on 9/18/19.
+//  Copyright © 2019 David Hoy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
